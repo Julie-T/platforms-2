564c5277f87eaef8dc19e325303b44c121504001
Juliya Trifonova <yula_sakura@list.ru>
2022-05-16 01:08:24 +0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-function info() {
-    const m = new Map();
-    m.set('name', 'ajs');
-    m.set('version', '1.0.0');
-    return m;
-  }
-  
-  export default {
-    info
-  };
+const ajs = require('@julie-t/platforms-1');
+
+console.log(ajs.info());
